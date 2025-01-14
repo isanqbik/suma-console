612fb3287477e49884265786970f4f368330116d
Luis <isan.qbik@gmail.com>
2025-01-14 11:43:54 +0100

    Add project files.

---
+++ suma-console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escribe el primer número:");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Escribe el segundo número:");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        int suma = Sumar(numero1, numero2);
+        Console.WriteLine($"La suma de {numero1} y {numero2} es {suma}");
+    }
+
+    static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ test-suma-console/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace test_suma_console;
+using Xunit;
+
+public class ProgramTests
+{
+    [Fact]
+    public void Sumar_DosNumeros_RetornaSuma()
+    {
+        // Arrange
+        int numero1 = 5;
+        int numero2 = 3;
+
+        // Act
+        int resultado = Program.Sumar(numero1, numero2);
+
+        // Assert
+        Assert.Equal(8, resultado);
+    }
+}
